c9e3592a7bdb393d369e16fed2d4ef523164fcdc
Muneeb Afsar <muneebafsar502@gmail.com>
2026-07-22 22:00:49 -0700

    add autotable creation

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
-# from database import engine,Base
 from routers import student,classes, user, auth
 from exceptions import AppException, NotFoundException, UnauthorizedException, ForbiddenException
+from database import engine, Base
+from contextlib import asynccontextmanager
 
-
-app = FastAPI() # the engine has been started with name of app
+@asynccontextmanager
+async def lifespan(app):
+    Base.metadata.create_all(bind=engine)
+    yield
+app = FastAPI(lifespan=lifespan) # the engine has been started with name of app
 
 #Global Exception
 @app.exception_handler(AppException) 
@@ -24,4 +28,5 @@ async def exception_handler(request: Request, exc: AppException):
 app.include_router(student.router, prefix="/students", tags=["Students"]) # tags helps us in grouping in swagger UI
 app.include_router(classes.router, prefix="/classes", tags=["Classes"])
 app.include_router(user.router, prefix="/users", tags=["Users"]) 
-app.include_router(auth.router, prefix="/auth", tags=["Authentication"]) 
+app.include_router(auth.router, prefix="/auth", tags=["Authentication"])
+
